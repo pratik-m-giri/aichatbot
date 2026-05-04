8dac7b143013bd7de838467cfb84cf3b73649a50
pratikgir99 <gpratik9973@gmail.com>
2026-05-04 10:20:59 +0530

    Clean commit without secrets

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMXH7ssJ3jRJzqA6qbnX7OBwneCvFsXzI",
+  authDomain: "ai-interview-e94b0.firebaseapp.com",
+  projectId: "ai-interview-e94b0",
+  storageBucket: "ai-interview-e94b0.firebasestorage.app",
+  messagingSenderId: "1091010501138",
+  appId: "1:1091010501138:web:ae121dde53635bea08d6cb"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };

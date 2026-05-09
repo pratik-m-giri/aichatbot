20d22999eea043d4eac1ff8c8533c711ff0eca9e
pratikgir99 <gpratik9973@gmail.com>
2026-05-09 16:00:32 +0530

    Updated project changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Auth from './pages/Auth'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
@@ -10,6 +9,7 @@ import InterviewPage from './pages/InterviewPage'
 import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
+import Auth from './pages/Auth'
 
 export const ServerUrl  = "http://localhost:8000"
 
